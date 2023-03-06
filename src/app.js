eccb9a57e6f4b0a3a787106af6f2a4c2dfe20ab1
Mvferrof <mvferrof@gmail.com>
2023-03-06 20:01:03 +0000

    "change app.js , index.html and styles.css"

--- src/app.js
+++ src/app.js
@@ -4,18 +4,21 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
-window.onload = function() {
   let suitTop = document.querySelector(".suit-top");
   let suitBottom = document.querySelector(".suit-bottom");
   let cardValue = document.querySelector(".card-value");
+  let changeCardButton = document.querySelector(".change-card");
 
   let cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
-  let suitValues = ["♠", "♦", "♥", "♣"];
+  let suitValues = ["♣", "♥", "♠", "♦"];
 
   function generateRandomValues() {
     const generatedRandomSuit = Math.floor(Math.random() * suitValues.length);
@@ -29,12 +32,25 @@ window.onload = function() {
     if (generatedRandomSuit % 2 === 0) {
       suitBottom.classList.add("black");
       suitTop.classList.add("black");
+      cardValue.classList.add("black");
     } else {
       suitBottom.classList.add("red");
       suitTop.classList.add("red");
+      cardValue.classList.add("red");
+
       suitBottom.classList.remove("black");
       suitTop.classList.remove("black");
+      cardValue.classList.remove("black");
     }
   }
+
   generateRandomValues();
+
+  changeCardButton.addEventListener("click", generateRandomValues);
+
+  setTimeout(() => {
+    generateRandomValues();
+  }, 10000);
+
+  console.log("Hello Rigo from the console!");
 };
